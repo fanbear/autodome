f020b8cff0a37781602bae965d5bd24dd89eef34
Yura <fanbearytb@gmail.com>
2021-07-27 18:30:57 +0300

    no adaptive

--- build/js/common.js
+++ build/js/common.js
@@ -7,6 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchList = document.querySelector('.header-search__category .dropdown-menu');
     const searchBtn = document.querySelector('.header-search__open-category');
 
+    //Табы
+    const tabsHeader = document.querySelectorAll('.product-tabs__header-title');
+    const tabsContent = document.querySelectorAll('.product-tabs__content-wrapper');
+
+
+
+
     // elem - элемент который нужно скрыть/отобразить
     // btn - кнопка (img) которой нужно задать действие при открытии
     // вложеность для кнопки например: <span><img src="arrow"></span>
@@ -21,10 +28,41 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
+    //Модуль табов
+    if (tabsHeader) {
 
+        // Удаление класса из псевдомасива
+        // array - колекция DOM елементов
+        // selectro - имя класса для удаления
+        function deactiveTabs( array, selector ) {
+            array.forEach( function ( item ){
+                item.classList.remove(selector);
+            })
+        }
+
+        //Изменение табов по нажатию
+        tabsHeader.forEach( function(item, i) {
+
+            item.addEventListener('click', function() {
+
+                deactiveTabs(tabsHeader, 'product-tabs__header-active')
+                deactiveTabs(tabsContent, 'product-tabs__content-active')
+                this.classList.add('product-tabs__header-active');
+                tabsContent[i].classList.add('product-tabs__content-active')
+                
+            })
+        })
+    }
+    
+
+
+
+    //Открытие адерса в шапке
     addressBtn.addEventListener('click', function() {
         toggleSelector(address, addressBtn)
     })
+
+    //Открытие категорий в поиске
     searchBtn.addEventListener('click', function() {
         toggleSelector(searchList, searchBtn)
     })
